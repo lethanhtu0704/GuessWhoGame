3920167ab3c14e2c4d030dec0b8cd18c14cd0fdc
Le <tuthanh.le@dxc.com>
2025-03-10 19:11:52 +0700

    update package

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { ghPages } from 'vite-plugin-gh-pages';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // plugins: [react()],
   plugins: [ghPages()],
   base:"GuessWhoGame"
 })
